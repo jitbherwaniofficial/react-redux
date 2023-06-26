fef478e2d2514da55797447f647fee8cf8b9cf83
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-26 13:49:08 +0530

    React_redux_toolkit

---
+++ src/Components/Product.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/slices/cartSlice";
+
+const Product = (props) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="card" style={{ width: "18rem;", margin: "10px" }}>
+      <img className="card-img-top" src={props.image} alt={props.productName} />
+      <div className="card-body">
+        <h5 class="card-title">{props.productName}</h5>
+        <p class="card-text">Rs. {props.price}/-</p>
+        <button
+          className="btn btn-primary"
+          onClick={e => dispatch(addItem({name:props.productName, price: props.price}))}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
